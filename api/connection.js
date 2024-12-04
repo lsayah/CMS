29e64aa965cce5291a7010e6271eb9048ecc0a6a
Ruchdane AMADOU <ruchdaneabiodun@gmail.com>
2024-12-04 16:08:45 +0100

    init: ajout du projet api

---
+++ api/connection.js
@@ -0,0 +1,19 @@
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  queueLimit: 0,
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error("Erreur de connexion à la base de données :", err);
+    return;
+  }
+  console.log("Connexion  à la base de données établie avec succès.");
+});
+module.exports = connection;
