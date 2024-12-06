744cfebd5917e13644187553983741e6669a5f3d
Isayah <isayahshelby@gmail.com>
2024-12-06 09:40:15 +0100

    fix: Mise en place de la BDD

--- api/init.sql
+++ api/init.sql
@@ -1,119 +1,69 @@
-DROP TABLE IF EXISTS Users;
-
-CREATE TABLE Users (
-    ID_users_Users INT(100) AUTO_INCREMENT NOT NULL,
-    name_Users VARCHAR(100),
-    last_name_Users VARCHAR(100),
-    mail_Users VARCHAR(100),
-    role_Users VARCHAR(100),
-    status_Users VARCHAR(100),
-    username_Users VARCHAR(100),
-    PRIMARY KEY (ID_users_Users)
+CREATE TABLE users (
+    id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(100),
+    lastname VARCHAR(100),
+    mail VARCHAR(100),
+    role VARCHAR(100),
+    status VARCHAR(100),
+    username VARCHAR(100),
+    crypted_password VARCHAR(100),
+    PRIMARY KEY (id)
 ) ENGINE = InnoDB;
 
-DROP TABLE IF EXISTS Comment;
 
-CREATE TABLE Comment (
-    ID_comment_Comment BIGINT(100) AUTO_INCREMENT NOT NULL,
-    ID_users_Users * * NOT FOUND * *(100),
-    ID_post_Posts * * NOT FOUND * *(100),
-    PRIMARY KEY (ID_comment_Comment)
+CREATE TABLE posts (
+    id INT(100) AUTO_INCREMENT NOT NULL,
+    pictures VARCHAR(100),
+    title VARCHAR(100),
+    description VARCHAR(100),
+    published_at DATETIME,
+    published_by VARCHAR(100),
+    updated_at DATETIME,
+    id_user INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_user) REFERENCES users(id)
 ) ENGINE = InnoDB;
 
-DROP TABLE IF EXISTS Posts;
 
-CREATE TABLE Posts (
-    ID_post_Posts INT(100) AUTO_INCREMENT NOT NULL,
-    pictures_Posts VARCHAR(100),
-    titles_Posts VARCHAR(100),
-    description_Posts VARCHAR(100),
-    published_at_Posts VARCHAR(100),
-    published_by_Posts VARCHAR(100),
-    updated_at_Posts VARCHAR(100),
-    ID_users_Users * * NOT FOUND * *(100),
-    PRIMARY KEY (ID_post_Posts)
+CREATE TABLE tags (
+    id INT(100) AUTO_INCREMENT NOT NULL,
+    name VARCHAR(100),
+    description VARCHAR(100),
+    PRIMARY KEY (id)
 ) ENGINE = InnoDB;
 
-DROP TABLE IF EXISTS Tags;
 
-CREATE TABLE Tags (
-    ID_tags_Tags INT(100) AUTO_INCREMENT NOT NULL,
-    categories_Tags VARCHAR(100),
-    PRIMARY KEY (ID_tags_Tags)
+
+
+CREATE TABLE posts_contain_tags (
+    id_post INT NOT NULL,
+    id_tag INT NOT NULL,
+    PRIMARY KEY (id_post, id_tag),
+    FOREIGN KEY (id_post) REFERENCES posts(id),
+    FOREIGN KEY (id_tag) REFERENCES tags(id)
 ) ENGINE = InnoDB;
 
-DROP TABLE IF EXISTS Reply_Comment;
 
-CREATE TABLE Reply_Comment (
-    ID_reply_Reply_Comment VARCHAR(100) AUTO_INCREMENT NOT NULL,
-    PRIMARY KEY (ID_reply_Reply_Comment)
+CREATE TABLE comments (
+    id INT(100) AUTO_INCREMENT NOT NULL,
+    id_users INT NOT NULL,
+    id_post INT NOT NULL,
+    id_comment_parent INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_users) REFERENCES users(id),
+    FOREIGN KEY (id_post) REFERENCES posts(id),
+    FOREIGN KEY (id_comment_parent) REFERENCES comments(id)
+
 ) ENGINE = InnoDB;
 
-DROP TABLE IF EXISTS contain;
 
-CREATE TABLE contain (
-    ID_post_Posts * * NOT FOUND * *(100) AUTO_INCREMENT NOT NULL,
-    ID_tags_Tags * * NOT FOUND * *(100) NOT NULL,
-    PRIMARY KEY (ID_post_Posts, ID_tags_Tags)
+
+CREATE TABLE users_choose_tags (
+    id_user INT NOT NULL,
+    id_tag INT NOT NULL,
+    PRIMARY KEY (id_user, id_tag),
+    FOREIGN KEY (id_user) REFERENCES users(id),
+    FOREIGN KEY (id_tag) REFERENCES tags(id)
 ) ENGINE = InnoDB;
 
-DROP TABLE IF EXISTS reply;
 
-CREATE TABLE reply (
-    ID_comment_Comment * * NOT FOUND * *(100) AUTO_INCREMENT NOT NULL,
-    ID_reply_Reply_Comment * * NOT FOUND * *(100) NOT NULL,
-    PRIMARY KEY (ID_comment_Comment, ID_reply_Reply_Comment)
-) ENGINE = InnoDB;
-
-DROP TABLE IF EXISTS choose;
-
-CREATE TABLE choose (
-    ID_users_Users * * NOT FOUND * *(100) AUTO_INCREMENT NOT NULL,
-    ID_tags_Tags * * NOT FOUND * *(100) NOT NULL,
-    PRIMARY KEY (ID_users_Users, ID_tags_Tags)
-) ENGINE = InnoDB;
-
-ALTER TABLE
-    Comment
-ADD
-    CONSTRAINT FK_Comment_ID_users_Users FOREIGN KEY (ID_users_Users) REFERENCES Users (ID_users_Users);
-
-ALTER TABLE
-    Comment
-ADD
-    CONSTRAINT FK_Comment_ID_post_Posts FOREIGN KEY (ID_post_Posts) REFERENCES Posts (ID_post_Posts);
-
-ALTER TABLE
-    Posts
-ADD
-    CONSTRAINT FK_Posts_ID_users_Users FOREIGN KEY (ID_users_Users) REFERENCES Users (ID_users_Users);
-
-ALTER TABLE
-    contain
-ADD
-    CONSTRAINT FK_contain_ID_post_Posts FOREIGN KEY (ID_post_Posts) REFERENCES Posts (ID_post_Posts);
-
-ALTER TABLE
-    contain
-ADD
-    CONSTRAINT FK_contain_ID_tags_Tags FOREIGN KEY (ID_tags_Tags) REFERENCES Tags (ID_tags_Tags);
-
-ALTER TABLE
-    reply
-ADD
-    CONSTRAINT FK_reply_ID_comment_Comment FOREIGN KEY (ID_comment_Comment) REFERENCES Comment (ID_comment_Comment);
-
-ALTER TABLE
-    reply
-ADD
-    CONSTRAINT FK_reply_ID_reply_Reply_Comment FOREIGN KEY (ID_reply_Reply_Comment) REFERENCES Reply_Comment (ID_reply_Reply_Comment);
-
-ALTER TABLE
-    choose
-ADD
-    CONSTRAINT FK_choose_ID_users_Users FOREIGN KEY (ID_users_Users) REFERENCES Users (ID_users_Users);
-
-ALTER TABLE
-    choose
-ADD
-    CONSTRAINT FK_choose_ID_tags_Tags FOREIGN KEY (ID_tags_Tags) REFERENCES Tags (ID_tags_Tags);
