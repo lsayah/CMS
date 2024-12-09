d893d8f43c6c33c2fd7b9ac621af33118370b347
Mallo <mallo@MacBook-Pro-de-Mallo.local>
2024-12-09 15:45:18 +0100

    La page Log In mise en place

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,30 @@
+import React from "react";
+import "../styles/Login.css";
+import Button from "../components/button.jsx";
+import InputField from "../components/InputField.jsx";
 export default function Login() {
   return (
     <div>
-      <h1>Login</h1>
+      <div class="logo-container">
+        <div class="rectangle"></div>
+      </div>
+      <div class="login-container">
+        <h1>Login</h1>
+        <h3>Email :</h3>
+        <InputField type="email" placeholder="Email" />
+        <h3>Password :</h3>
+        <InputField type="password" placeholder="Password" />
+        <div>
+          <input
+            type="checkbox"
+            id="RememberMe"
+            name="interest"
+            value="RememberMe"
+          />
+          <label for="coding">Remember me</label>
+        </div>
+        <Button text="Log In" />
+      </div>
     </div>
   );
 }
