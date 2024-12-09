053898189e2e0532737782b477ec1710dc1583ad
Mallo <mallo@MacBook-Pro-de-Mallo.local>
2024-12-09 12:18:18 +0100

    init: mise en place du routage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import Feed from "./pages/Feed";
+import Post from "./pages/Post";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/post" element={<Post />} />
+        <Route path="/feed" element={<Feed />} />
       </Routes>
     </BrowserRouter>
   );
 }
+
+export default App;
