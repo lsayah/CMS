80de2ac4e75a707d404c969fe0ea70bd4fa37e17
ruchdane <ruchdaneabiodun@gmail.com>
2024-12-20 11:52:29 +0100

    wip

--- client/src/components/ImagePicker/ImagePicker.jsx
+++ client/src/components/ImagePicker/ImagePicker.jsx
@@ -1,11 +1,12 @@
-import "./ImagePicker.css"
-import Image from "../../assets/icons/Image.svg"
+import "./ImagePicker.css";
+import Image from "../../assets/icons/Image.svg";
 
-export default function ImagePicker() {
-  return <div>
-    <input type="file" placeholder="Click or drag an image" />
-    <div className="image-picker placeholder">
-      <img src={Image} width="64" height="64" />
+export default function ImagePreview() {
+  return (
+    <div>
+      <div className="image-picker placeholder">
+        <img src={Image} width="64" height="64" />
+      </div>
     </div>
-  </div>
+  );
 }
--- client/src/pages/Post/CreatePost.jsx
+++ client/src/pages/Post/CreatePost.jsx
@@ -1,9 +1,9 @@
-import "./Post.css"
+import "./Post.css";
 import { PlusCircle } from "lucide-react";
 import LabelInput from "../../components/InputField";
 import PostEditor from "../../components/PostEditor/PostEditor";
 import TagSelector from "../../components/TagSelector/TagSelector";
-import ImagePicker from "../../components/ImagePicker/ImagePicker";
+import ImagePreview from "../../components/ImagePicker/ImagePicker";
 import Button from "../../components/Button";
 
 export default function CreatePost() {
@@ -12,19 +12,24 @@ export default function CreatePost() {
       <h1>Create New Post</h1>
       <p> Start a conversation, a blog, or share a topic. </p>
       <div className="post-meta">
-        <LabelInput id="post_title" label="Title" />
-        <div className="label-input" >
-          <label className="label-input" htmlFor="post_image"> Choose post image </label>
-          <ImagePicker id="post_image" />
+        <div>
+          <LabelInput id="post_title" label="Title" />
+          <LabelInput id="post_title" label="Choose post image" type="file" />
+        </div>
+        <div className="label-input">
+          <ImagePreview id="post_image" />
         </div>
       </div>
-      <div className="label-input" >
-        <label className="label-input" htmlFor="post_content"> Content </label>
+      <div className="label-input">
+        <label className="label-input" htmlFor="post_content">
+          {" "}
+          Content{" "}
+        </label>
         <PostEditor id="post_content" />
       </div>
       <div className="create-post-footer">
-        <div className="label-input" >
-          <label htmlFor="post_tags">   Choose post categories </label>
+        <div className="label-input">
+          <label htmlFor="post_tags"> Choose post categories </label>
           <TagSelector id="post_tags">
             <PlusCircle />
             <span> Ajouter </span>
