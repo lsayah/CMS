4c8e007f70b2300b315dd0ac5d2872ef9f72207c
ruchdane <ruchdaneabiodun@gmail.com>
2024-12-20 12:19:54 +0100

    feat: add dist serve for frontend

--- api/app.js
+++ api/app.js
@@ -11,50 +11,59 @@ import { getJWTconfig } from "./routes/middleware.js";
 import { expressjwt as jwt } from "express-jwt";
 
 import "./connection.js";
-import indexRouter from "./routes/index.js";
 import usersRouter from "./routes/users.js";
 import postRouter from "./routes/posts.js";
 import tagsRouter from "./routes/tags.js";
 import authRouter from "./routes/authentification.js";
 import commentsRouter from "./routes/comments.js";
+
 const file = readFileSync("./api.yml", "utf8");
 const swaggerDocument = parse(file);
+
 config();
-var app = express();
 const jwtConfig = getJWTconfig();
+
+var app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 app.use(logger("dev"));
 app.use(json());
 app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(staticFile(join(dirname(fileURLToPath(import.meta.url)), "public")));
+
 app.use(
+  "/api",
   jwt(jwtConfig).unless({
     path: [
       new RegExp("/api/doc/*"),
       "/api/auth/login",
-      {
-        url: "/api/posts",
-        methods: ["GET"],
-      },
-      {
-        url: "/api/tags",
-        methods: ["GET"],
-      },
-      {
-        url: "/api/users",
-        methods: ["POST"],
-      },
+      { url: "/api/posts", methods: ["GET"] },
+      { url: "/api/tags", methods: ["GET"] },
+      { url: "/api/users", methods: ["POST"] },
     ],
   })
 );
+
 app.use("/api/auth", authRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/posts", postRouter);
 app.use("/api/comments", commentsRouter);
 app.use("/api/tags", tagsRouter);
-app.use("/", indexRouter);
+
 app.use("/api/doc", serve, setup(swaggerDocument));
+
+app.use("/api/*", (req, res) => {
+  res.status(404).json({ error: "API endpoint not found" });
+});
+
+app.use(express.static(join(__dirname, "client")));
+
+app.get("*", (req, res) => {
+  res.sendFile(join(__dirname, "client", "index.html"));
+});
+
 app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.json({ error: err.message });
